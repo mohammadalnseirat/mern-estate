d2d4ecec1f5defc38470d6faea9c8721ae72e51b
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-06-27 15:44:35 +0400

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import About from "./pages/About";
 const App = () => {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>}/>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />

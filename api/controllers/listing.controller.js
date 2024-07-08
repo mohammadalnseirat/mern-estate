981ced4df5de2540115b2a5b07f1aee895603504
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-08 21:53:00 +0400

    create delete listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,34 @@
 import Listing from "../models/listing.model.js";
+import { handleErrors } from "../utils/error.js";
 
 // function to create a new listing:
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// function to delete a listing:
+export const deleteListing = async (req, res, next) => {
+  // get the listing:
+  const listing = await Listing.findById(req.params.id);
+
+  // check if the listing is existing:
+  if (!listing) {
+    return next(handleErrors(404, "Listing not found!"));
+  }
+
+  // check if the user is the owner of the listing:
+  if (req.user.id !== listing.userRef) {
+    return next(handleErrors(401, "You can only delete your own listings!"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Listing has been deleted successfully!");
   } catch (error) {
     next(error);
   }

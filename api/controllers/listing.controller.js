3cf876a6b834f93b58e3a45cd071aeaf478e22fa
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-19 03:39:04 +0400

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -75,3 +75,57 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+// function for search :
+export const getListings = async (req, res, next) => {
+  try {
+    // 1- Add Limit and startIndex to request:
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    // 2-search based on offer
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    // 3-search based on furnished:
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    //4- search based on parking:
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    // 5-search based on type:
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    // 6-search based on search Term and sort and order:
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    // 7-get listing after get all queries:
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
